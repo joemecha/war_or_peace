2c1dd933af48cadb81fb6fc543afa3097e60b019
joemecha <jwmecha@gmail.com>
2021-02-02 20:43:54 -0600

    Add @values to deck instance variables. Currently populates a deck with 676 cards.

--- lib/deck.rb
+++ lib/deck.rb
@@ -4,32 +4,35 @@ class Deck
   def initialize
     @cards = []
     @ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+    @values = %w(two three four five six seven eight nine ten jack queen king ace)
     @suits = [:club, :diamond, :heart, :spade]
 
     @ranks.each do |rank|
-      @suits.each do |suit|
-        @cards << Card.new(rank, suit)
-      end
+      @values.each do |value|
+        @suits.each do |suit|
+          @cards << Card.new(suit, value, rank)
+        end
+      end 
     end
   end
 
-  def rank_of_card_at(index)
-    #stuff
-  end
-
-  def high_ranking_cards(cards)
-    #returns an array of cards in deck rank 11 and above
-  end
-
-  def percent_high_ranking(cards)
-    #this method will return the percentage of high ranking cards
-  end
-
-  def remove_card
-    #removes top card of deck
-  end
-
-  def add_card
-    #adds one card to the bottom (end) of the deck
-  end
+  # def rank_of_card_at(index)
+  #   # stuff
+  # end
+  #
+  # def high_ranking_cards(cards)
+  #   # returns an array of cards in deck rank 11 and above
+  # end
+  #
+  # def percent_high_ranking(cards)
+  #   # this method will return the percentage of high ranking cards
+  # end
+  #
+  # def remove_card
+  #   # removes top card of deck
+  # end
+  #
+  # def add_card
+  #   # adds one card to the bottom (end) of the deck
+  # end
 end
