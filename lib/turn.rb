61612377362644747e411d26feede41c83a65494
joemecha <jwmecha@gmail.com>
2021-02-03 21:57:57 -0600

    Created turn_test with 'setup'. Began to write class Turn.

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+class Turn
+  attr_reader :player1, :player2, :spoils_of_war
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @spoils_of_war = []
+  end
+
+  def type
+    # stuff
+  end
+
+  def winner
+    #stuff
+  end
+
+  def pile_cards
+    #stuff
+  end
+
+  def award_spoils
+    #stuff
+  end 
+end
