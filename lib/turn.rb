56d6819949bbbc8204d851051cf1b71a8f490a24
joemecha <jwmecha@gmail.com>
2021-02-04 15:29:40 -0600

    Finished tests and code for 'type' and 'winner'

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,21 +5,46 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @types = [:basic, :war, :mutually_assured_destruction]
   end
 
   def type
-    # stuff
+    # Compare cards from players' decks to determine type of turn
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      @types[0]
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
+          @player1.deck.rank_of_card_at(2) != @player2.deck.rank_of_card_at(2)
+      @types[1]
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0) &&
+          @player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2)
+      @types[2]
+    end
   end
 
   def winner
-    #stuff
+    #Determine the winner of the turn
+    if type == :basic
+      if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
+        @player1
+      else
+        @player2
+      end
+    elsif type == :war
+      if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
+        @player1
+      else
+        @player2
+      end 
+    elsif type == :mutually_assured_destruction
+      "No Winner"
+    end
   end
 
   def pile_cards
-    #stuff
+    #Cards sent from players' decks into spoils_of_war
   end
 
   def award_spoils
-    #stuff
-  end 
+    #Cards in spoils_of_war added to winner's deck
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -28,5 +28,107 @@ class TurnTest < Minitest::Test
 
   def test_a_turn_exists
     assert_instance_of Turn, @turn
-  end 
+  end
+
+  def test_a_turn_has_players
+    assert_equal @player1, @turn.player1
+    assert_equal @player2, @turn.player2
+  end
+
+  def test_spoils_of_war_starts_empty
+    assert_equal [], @turn.spoils_of_war
+  end
+
+  def test_type_basic_if_top_cards_different_rank
+    assert_equal :basic, @turn.type
+  end
+
+  def test_type_war_if_top_cards_different_rank
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, 'Jack', 11)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal :war, @turn.type
+  end
+
+  def test_type_MAD_if_top_cards_different_rank
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, 'Jack', 11)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, '8', 8)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal :mutually_assured_destruction, @turn.type
+  end
+
+  def test_player_higher_ranked_top_card_is_winner
+    assert_equal @player1, @turn.winner
+  end
+
+  def test_player_with_higher_ranked_third_card_wins_war_type_turn
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, 'Jack', 11)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, 'Queen', 12)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal @player2, @turn.winner
+  end
+
+  def test_no_winner_for_MAD_type_turn
+    @card1 = Card.new(:heart, 'Jack', 11)
+    @card2 = Card.new(:heart, '10', 10)
+    @card3 = Card.new(:heart, 'Jack', 11)
+    @card4 = Card.new(:diamond, 'Jack', 11)
+    @card5 = Card.new(:heart, '8', 8)
+    @card6 = Card.new(:diamond, '8', 8)
+    @card7 = Card.new(:heart, '3', 3)
+    @card8 = Card.new(:diamond, '2', 2)
+
+    @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+    @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+
+    @turn = Turn.new(@player1, @player2)
+
+    assert_equal "No Winner", @turn.winner
+  end
+
 end
