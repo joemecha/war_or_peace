ced423ce52184c3ec10972664e921e7d7426e25c
joemecha <jwmecha@gmail.com>
2021-02-05 22:04:37 -0600

    Rewrite or refactor methods for generating standard deck, shuffle. 'deal_cards' not working. Begin writing method 'start'

--- lib/game.rb
+++ lib/game.rb
@@ -1,35 +1,50 @@
 class Game
+  attr_reader :cards, :deck1, :deck2
 
   def initialize
-    
+    @cards = []
+    @deck1 = []
+    @deck2 = []
   end
 
   def standard_deck
-    @ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    @values = %w(two three four five six seven eight nine ten jack queen king ace)
-    @suits = [:club, :diamond, :heart, :spade]
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    values = %w(two three four five six seven eight nine ten jack queen king ace)
+    suits = [:club, :diamond, :heart, :spade]
 
-    @ranks_and_value_hash = Hash[@ranks.zip(@values)] # create hash to make this work
+    ranks_and_value_hash = Hash[ranks.zip(values)] # create hash to make this work
 
-    @ranks_and_value_hash.each do |rank, value|
-      @suits.each do |suit|
+    ranks_and_value_hash.each do |rank, value|
+      suits.each do |suit|
         @cards << Card.new(suit, value, rank)
       end
     end
   end
 
   def shuffle_deck
-    standard_deck.shuffle
+    @cards.shuffle!
   end
 
   def deal_cards
-    @cards.each do |card|
-      if card.index.even?
-        @turn.player1.deck.cards << card
-      elsif card.index.odd?
-        @turn.player2.deck.cards << card
-      end
-    end
+    shuffle_deck
+    @deck1 = @cards[0..25]
+    @deck2 = @cards[26..52]
+
+  end
+
+  def start
+
+    @player1 = Player.new("Muttley", @deck1)
+    @player2 = Player.new("Underdog", @desk2)
   end
 
 end
+
+
+#rewrite [0..]
+# @cards.each do |card|
+#   if @cards.index(card).even?
+#     @deck1 << card
+#   elsif @cards.index(card).odd?
+#     @deck2 << card
+#   end
--- test/game_test.rb
+++ test/game_test.rb
@@ -5,6 +5,8 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 require './lib/game'
+# require './war_or_peace_runner'
+require 'pry'
 
 class GameTest < Minitest::Test
   def setup
@@ -12,15 +14,33 @@ class GameTest < Minitest::Test
   end
 
   def test_game_exists
-    @game = Game.new
-
     assert_instance_of Game, @game
   end
 
   def test_standard_deck_has_52_cards
-
+    # binding.pry
     @game.standard_deck
 
     assert_equal @game.cards.length, 52
+  end
+
+  def test_shuffle_deck
+    @game.standard_deck
+    unshuffled = @game.cards[0..2]
+
+    @game.shuffle_deck
+    shuffled = @game.cards[0..2]
+
+    refute_equal shuffled, unshuffled
+  end
+
+  def test_deal_cards
+    skip
+    @game.standard_deck
+    @game.deal_cards
+
+    assert_equal @deck1.length, 26
+    assert_equal @deck2.length, 26
+    refute_equal @deck1, @deck2
   end
 end
